390ff4c84c30d3802d6cea87d04421a4a64818a6
admin <baselqarb@gmail.com>
2024-03-20 15:19:14 +0100

    Initial commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public record Animal(int ID, String Name, String Species, int Age) {
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        BigDecimal nr = new BigDecimal("1.23123");
+//        System.out.println(nr.setScale(2, RoundingMode.DOWN));
+//        System.out.println(nr.setScale(2, RoundingMode.UP));
+//        System.out.println(nr.setScale(2, RoundingMode.CEILING));
+
+
+    }
+}
